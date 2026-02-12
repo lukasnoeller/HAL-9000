e63386462521437752d2ee55d2231555e2e034f5
Lukas <lukasnoller@gmail.com>
2026-02-12 18:41:37 +0100

    Make model loading occur once at startup

--- stream.py
+++ stream.py
@@ -1,25 +1,101 @@
-import os
-import time
+import sys
+import pyaudio
 import torch
-import torchaudio
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
-
-import sounddevice as sd
-import soundfile as sf
-import numpy as np
-import threading
-import time
-import queue  # Use a queue for thread-safe data transfer
 import argparse
 
+
+def load_model():
+    print("Loading model...")
+    config = XttsConfig()
+    config.load_json("config.json")
+    model = Xtts.init_from_config(config)
+    model.load_checkpoint(
+        config,
+        checkpoint_dir="xtts-streaming/data/checkpoints/xtts_v2",
+        use_deepspeed=False,
+    )
+    model.cuda()
+
+    speaker = {
+        "speaker_embedding": model.speaker_manager.speakers["Hal-9000"][
+            "speaker_embedding"
+        ]
+        .cpu()
+        .squeeze()
+        .half()
+        .tolist(),
+        "gpt_cond_latent": model.speaker_manager.speakers["Hal-9000"]["gpt_cond_latent"]
+        .cpu()
+        .squeeze()
+        .half()
+        .tolist(),
+    }
+
+    speaker_embedding = (
+        torch.tensor(speaker.get("speaker_embedding")).unsqueeze(0).unsqueeze(-1)
+    )
+    gpt_cond_latent = (
+        torch.tensor(speaker.get("gpt_cond_latent")).reshape((-1, 1024)).unsqueeze(0)
+    )
+
+    return model, gpt_cond_latent, speaker_embedding
+
+
+def generate_audio(text, model, gpt_cond_latent, speaker_embedding):
+    # file_path = os.path.join("..", "reference_audio_files", "HAL9000_Voice_noise_reduced.wav")
+    # encoded_audio_file = encode_audio(file_path)
+
+    FORMAT = pyaudio.paInt16  # Audio format (e.g., 16-bit PCM)
+    CHANNELS = 1  # Number of audio channels
+    RATE = 24000  # Sample rate
+
+    # Initialize PyAudio
+    p = pyaudio.PyAudio()
+
+    # Open a stream for audio playback
+    stream = p.open(format=pyaudio.paFloat32, channels=CHANNELS, rate=RATE, output=True)
+
+    print("Generating audio...")
+    # Create a buffer to hold multiple chunks
+    buffer = b""
+    buffer_size_threshold = 2**20
+    streamer = model.inference_stream(
+        text,
+        "en",
+        gpt_cond_latent,
+        speaker_embedding,
+        stream_chunk_size=4096,
+        enable_text_splitting=True,
+        temperature=0.2,
+    )
+    # Stream and play the audio data as it's received
+    for chunk in streamer:
+        if chunk is not None:
+            chunk_bytes = chunk.cpu().numpy().tobytes()
+            buffer += chunk_bytes
+            if len(buffer) >= buffer_size_threshold:
+                print(f"Writing buffer of size: {len(buffer)}")
+                stream.write(buffer)
+                buffer = b""  # Clear the buffer
+            # stream.write(chunk)
+    if buffer:
+        print(f"Writing final buffer of size: {len(buffer)}")
+        stream.write(buffer)
+
+    # if buffer:
+    #     print(f"Writing final buffer of size: {len(buffer)}")
+    #     stream.write(buffer)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Stream audio from XTTS with text input.")
+    parser = argparse.ArgumentParser(
+        description="Stream audio from XTTS with text input."
+    )
     parser.add_argument(
-        '-i',
-        '--interactive',
-        action='store_true',
-        help='Enable interactive mode')
+        "-i", "--interactive", action="store_true", help="Enable interactive mode"
+    )
     parser.add_argument(
         "text",
         nargs="?",  # Allow for optional text argument
@@ -27,80 +103,19 @@ def main():
         default=None,  # Default to None if no text provided
     )
 
+    model, gpt_cond_latent, speaker_embedding = load_model()
     args = parser.parse_args()
 
-    # Get text input (from argument or stdin)
     if not args.interactive:
-        print("Shutting down!")
-        #text = args.text
+        if not args.text:
+            print("Shutting down!")
+            sys.exit(0)
+        else:
+            generate_audio(args.text, model, gpt_cond_latent, speaker_embedding)
     else:
-        #text = input("Enter text (or pipe text in): ")
-        # Load audio data and sample rate
-        data, sr = sf.read("HAL9000 just a moment_background_reduced.mp3", dtype='float32')
-
-        sd.play(data, sr)
-        print("Loading model...")
-        config = XttsConfig()
-        config.load_json("config.json")
-        model = Xtts.init_from_config(config)
-        model.load_checkpoint(config, checkpoint_dir=".", use_deepspeed=True)
-        model.cuda()
-
-        print("Computing speaker latents...")
-        gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=["audio_files/im_afraid.ogg"])
-        sd.play(data, sr)
         while True:
-            user_input = input("Enter your text (or type 'quit' to exit): ")
-            if user_input.lower() == "quit":
-                print("Goodbye!")
-                break
-            print("Inference...")
-            t0 = time.time()
-            print(f"Token length = {len(user_input)}")
-            # if len(user_input) > 250:
-            #     print("ERROR: TOO MANY TOKENS")
-            chunks = model.inference_stream(
-                user_input,
-                "en",
-                gpt_cond_latent,
-                speaker_embedding
-            )
-
-
-            
-            chunks_buffer = []
-            wav_chunks = []
-            with sd.OutputStream(samplerate=24000, channels=1, dtype='float32') as stream:
-                for i, chunk in enumerate(chunks):
-                    if i == 0:
-                        print(f"Time to first chunk: {time.time() - t0}")
-                    print(f"Received chunk {i} of audio length {chunk.shape[-1]}")
-                    chunks_buffer.append(chunk)
-                    wav_chunks.append(chunk)
-                    if len(chunks_buffer) < 5:
-                        # Buffer until you have at least 2 chunks
-                        continue
-                    
-                    if len(chunks_buffer) == 5:
-                        # Start playback by writing both buffered chunks
-                        for buffered_chunk in chunks_buffer:
-                            stream.write(buffered_chunk.squeeze().cpu().numpy())
-                        chunks_buffer = []
-                   #
-                        # For subsequent chunks, write directly
-                    #stream.write(chunk.squeeze().cpu().numpy())
-                for buffered_chunk in chunks_buffer:
-                    stream.write(buffered_chunk.squeeze().cpu().numpy())
-
-                    
-            
-                    
-
-            for chunk in wav_chunks:
-                sd.play(chunk.squeeze().cpu().numpy(), 24000)
-                sd.wait() 
-            wav = torch.cat(wav_chunks, dim=0)
-            torchaudio.save("xtts_streaming.wav", wav.squeeze().unsqueeze(0).cpu(), 24000)
+            text = input("Enter text (or pipe text in): ")
+            generate_audio(text, model, gpt_cond_latent, speaker_embedding)
 
 
 if __name__ == "__main__":
